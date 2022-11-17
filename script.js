b109693419f3b2ad98ddfa7a0663c5d52bd5eb0f
soundy0115 <soundy0115@gmail.com>
2022-11-16 22:50:34 -0800

    added html, js, css files for 5x5 tictactoe game

---
+++ script.js
@@ -0,0 +1,104 @@
+/*global document*/
+/*eslint-env es6*/
+
+var statusDisplay = document.querySelector('.game--status');
+
+var gameActive = true;
+var currentPlayer = "X";
+var gameState = ["", "", "", "", "",
+                 "", "", "", "", "",
+                 "", "", "", "", "",
+                 "", "", "", "", "",
+                 "", "", "", "", ""];
+
+var winningMessage = () => `Player ${currentPlayer} has won!`;
+var drawMessage = () => `Game ended in a draw!`;
+var currentPlayerTurn = () => `${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+var winningConditions = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24],
+    [0, 5, 10, 15, 20],
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24],
+    [0, 6, 12, 18, 24],
+    [4, 8, 12, 16, 20]
+];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 11; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        let d = gameState[winCondition[3]];
+        let e = gameState[winCondition[4]];
+        if (a === '' || b === '' || c === '' || d === '' || e === '') {
+            continue;
+        }
+        if (a === b && b === c && c === d && d === e) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "",
+                 "", "", "", "", "",
+                 "", "", "", "", "",
+                 "", "", "", "", "",
+                 "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
